fc16bfa27897176a6ad4aad85b326fbec8d72ebe
ScoreLiveT <rainbowt0506@gmail.com>
2025-03-28 11:28:38 +0800

    feat: initial weather app project

--- app/src/main/java/com/rainbowt0506/philipplackner_weatherapp/MainActivity.kt
+++ app/src/main/java/com/rainbowt0506/philipplackner_weatherapp/MainActivity.kt
@@ -3,45 +3,15 @@ package com.rainbowt0506.philipplackner_weatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.rainbowt0506.philipplackner_weatherapp.ui.theme.PhilippLacknerWeatherAppTheme
+import com.rainbowt0506.philipplackner_weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            PhilippLacknerWeatherAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            WeatherAppTheme {
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhilippLacknerWeatherAppTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/rainbowt0506/philipplackner_weatherapp/domain/util/Resource.kt
@@ -0,0 +1,6 @@
+package com.rainbowt0506.philipplackner_weatherapp.domain.util
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T?): Resource<T>(data)
+    class Error<T>(message: String, data: T? = null): Resource<T>(data, message)
+}
